7ab71e165944fe2c89c5b543dc3ad36b7cd9d504
moba1 <moba2@protonmail.com>
2022-10-07 12:08:02 +0900

    release v2.0.0

--- main.go
+++ main.go
@@ -13,7 +13,7 @@ import (
 )
 
 var (
-	Version           = "1.0.0"
+	Version           = "2.0.0"
 	VersionPrerelease = "dev"
 	PluginVersion     = version.InitializePluginVersion(Version, VersionPrerelease)
 )
